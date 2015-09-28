db76e293cd15209aded2d0e4f7857e7d34979041
Luke Iannini <lukexi@me.com>
2015-09-28 14:48:36 -0700

    Getting render target size

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,18 @@
 module Main where
 
 import Graphics.VR.OpenVR
+import Graphics.GL.Pal
+import Graphics.UI.GLFW.Pal
 
 main :: IO ()
 main = do
-  putStrLn "Calling OpenVR"
-  system <- initOpenVR
-  putStrLn $ "Got system: " ++ show system
-  putStrLn "Called OpenVR"
+  putStrLn "Starting OpenVR"
+  mSystem <- initOpenVR
+
+  case mSystem of
+    Nothing -> putStrLn "Couldn't create OpenVR system :*("
+    Just system -> do
+      (w,h) <- getRenderTargetSize system
+      print (w,h)
+      putStrLn $ "Got system: " ++ show system
+  putStrLn "Done!"
--- src/Graphics/VR/OpenVR.hs
+++ src/Graphics/VR/OpenVR.hs
@@ -5,22 +5,31 @@
 module Graphics.VR.OpenVR where
 
 import Foreign
-import Foreign.Ptr
--- import Foreign.C
+-- import Foreign.Ptr
+import Foreign.C
 import qualified Language.C.Inline.Cpp as C
 import Control.Monad.Trans
 import Data.Monoid
 
--- Set up inline-c to 
+-- Set up inline-c to gain Cpp and Function Pointer abilities
 C.context (C.cppCtx <> C.funCtx)
+-- Import OpenVR
 C.include "openvr.h"
 C.include "stdio.h"
 
 newtype IVRSystem = IVRSystem { unIVRSystem :: Ptr () } deriving Show
 
+-- | Temporarily allocate an array of the given size, 
+-- pass it to a foreign function, then peek it before it is discarded
+
+withArray_ :: (Storable a) => Int -> (Ptr a -> IO ()) -> IO [a]
+withArray_ size action = allocaArray size $ \ptr -> do
+  _ <- action ptr
+  peekArray size ptr
+
 initOpenVR :: MonadIO m => m (Maybe IVRSystem)
 initOpenVR = liftIO $ do
-  systemPtr <- [C.block| void * {
+  systemPtr <- [C.block| void* {
     vr::HmdError eError = vr::HmdError_None;
     vr::IVRSystem *system = vr::VR_Init(&eError);
     if (system == NULL) {
@@ -33,3 +42,11 @@ initOpenVR = liftIO $ do
     } |]
 
   return $ if systemPtr == nullPtr then Nothing else Just (IVRSystem systemPtr)
+
+getRenderTargetSize :: IVRSystem -> IO (Word32, Word32)
+getRenderTargetSize (IVRSystem systemPtr) = 
+  C.withPtrs_ $ \(xPtr, yPtr) -> do
+    [C.block| void {
+      vr::IVRSystem *system = (vr::IVRSystem *)$(void* systemPtr);
+      system->GetRecommendedRenderTargetSize($(uint32_t* xPtr), $(uint32_t* yPtr));
+    }|]
